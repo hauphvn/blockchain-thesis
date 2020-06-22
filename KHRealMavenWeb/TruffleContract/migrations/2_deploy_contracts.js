b5d96407c5d869edeb3e2710fb23e890f1192392
DangAnhKiet <DangAnhKiet>
2020-06-23 01:02:49 +0700

    ok

--- KHRealMavenWeb/TruffleContract/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var DocumentRegistry = artifacts.require("./DocumentRegistry.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(DocumentRegistry,{from:"0xF21FBB0B188184dD75be672C0dbD02A303556d82"});
-};
--- KHRealMavenWeb/src/main/java/khreal/DTO/LandDTO.java
+++
@@ -1,9 +0,0 @@
-package khreal.DTO;
-//(address, string memory, string memory, uint, uint)
-public class LandDTO {
-//    private String address;
-//    private String district;
-//    private String street;
-//    private int id;
-//    private int
-}
